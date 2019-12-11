fd64104857422bbf47e082124abb43ac04fedf83
mass <mass@masss-mbp.home>
2019-12-11 20:32:01 +0000

    fix data structure for results

--- src/routes/Search/actions/index.js
+++ src/routes/Search/actions/index.js
@@ -16,37 +16,42 @@ export const onClick = (cities, dateFrom, dateTo) => {
       const promise = axios.get(`https://kiwicom-prod.apigee.net/v2/search?fly_from=${cities[i].coordinates}&date_from=${dateFrom}&date_to=${dateTo}&flight_type=round&adults=${cities[i].numberOfPeople}&vehicle_type=aircraft`, config)
       promises.push(promise)
     }
-    const trips = []
+
     Promise.all(promises)
       .then((results) => {
+        const trips = {}
         for (let i = 0; i < results.length; i++) {
-          const trip = results[i].data.data.map(trip => (
-            {
-              [trip.cityFrom]: {
-                cityFrom: trip.cityFrom,
-                cityTo: trip.cityTo,
-                price: trip.price,
-                local_departure: trip.local_departure,
-                local_arrival: trip.local_arrival
-              }
+          const city = results[i].data.data[0].cityFrom
+          const trips_by_city = results[i].data.data.map(trip => {
+            return {
+              cityFrom: trip.cityFrom,
+              cityTo: trip.cityTo,
+              price: trip.price,
+              local_departure: trip.local_departure,
+              local_arrival: trip.local_arrival
             }
-            ))
-          trips.push(trip)
-        }
-        //Recuperer une liste des destinations communes
-        let commonTrips;
-        for (let i = 1 ; i < trips.length; i++) {
-          commonTrips = lodash.intersectionBy(trips[i - 1], trips[i], 'cityTo');
+          })
+          trips[city] = trips_by_city
         }
 
-        const commonDestinations = commonTrips.map((item) => {
-          return Object.keys(item)[0]
-        })
-        console.log(commonDestinations)
-         //Pour chaque ville garder seulement les destinations commune
-        for(let i = 0; i < trips.length; i++) {
-          trips[i] = filterTrips(trips[i], commonDestinations)
+        //Recuperer une liste des destinations communes
+        let commonTrips;
+        for (let i = 1; i < cities.length; i++) {
+          let city1 = cities[i].name
+          let city2 = cities[i - 1].name
+          commonTrips = lodash.intersectionBy(trips[city1], trips[city2], 'cityTo');
         }
+        const commonDestinations = commonTrips.map((item) => {
+          return item.cityTo
+        })
+
+        for (let i = 0; i < cities.length; i++) {
+          let city = cities[i].name
+          trips[city] = trips[city].filter((trip) => {
+            return commonDestinations.includes(trip.cityTo)
+          })          
+        }
+
         const data = {
           commonDestinations: commonDestinations,
           trips: trips
@@ -56,9 +61,9 @@ export const onClick = (cities, dateFrom, dateTo) => {
   }
 }
 
-const filterTrips = (trips, commonDestinations) => {
-  trips.filter( (trip) => {
-    return commonDestinations.includes(trip.city)
-  })
+//const filterTrips = (trips, commonDestinations) => {
+  //trips.filter((trip) => {
+    //return commonDestinations.includes(trip.city)
+  //})
 
-}
+//}
