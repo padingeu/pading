9afd536f364a0a774521272798cd16e9976a4c9e
mass <mass@masss-MacBook-Pro.local>
2020-01-15 20:07:58 +0000

    change date restriction

--- src/routes/Search/actions/index.js
+++ src/routes/Search/actions/index.js
@@ -2,8 +2,17 @@ import axios from 'axios';
 import lodash from 'lodash';
 import { history } from '../../../index';
 export const onClick = (cities, dateFrom, dateTo) => {
+
+  // To calculate the time difference of two dates 
+  var differenceInTime = dateTo.getTime() - dateFrom.getTime(); 
+  
+  // To calculate the no. of days between two dates 
+  var differenceInDays = Math.trunc(differenceInTime / (1000 * 3600 * 24)); 
+  console.log('differenceInDays')
+  console.log(differenceInDays)
   dateFrom = dateFrom.toLocaleDateString()
   dateTo = dateTo.toLocaleDateString()
+  
   const promises = []
   return dispatch => {
     let config = {
@@ -13,7 +22,8 @@ export const onClick = (cities, dateFrom, dateTo) => {
       }
     }
     for (let i = 0; i < cities.length; i++) {
-      const promise = axios.get(`https://kiwicom-prod.apigee.net/v2/search?fly_from=${cities[i].coordinates}&date_from=${dateFrom}&date_to=${dateTo}&flight_type=round&adults=${cities[i].numberOfPeople}&vehicle_type=aircraft`, config)
+      const promise = axios.get(`https://kiwicom-prod.apigee.net/v2/search?fly_from=${cities[i].coordinates}&date_from=${dateFrom}&date_to=${dateFrom}&return_from=${dateTo}&max_stopovers=2&flight_type=round&nights_in_dst_from=${differenceInDays}&nights_in_dst_to=${differenceInDays}&adults=${cities[i].numberOfPeople}&vehicle_type=aircraft`,
+       config)
       promises.push(promise)
     }
 
@@ -22,25 +32,33 @@ export const onClick = (cities, dateFrom, dateTo) => {
         const trips = {}
         //Construction d un objet avec une liste de voyage
         for (let i = 0; i < results.length; i++) {
-          const city = results[i].data.data[0].cityFrom
-          const trips_by_city = results[i].data.data.map(trip => {
-            return {
-              cityFrom: trip.cityFrom,
-              cityTo: trip.cityTo,
-              price: trip.price,
-              local_departure: trip.local_departure,
-              local_arrival: trip.local_arrival
-            }
-          })
-          trips[city] = trips_by_city
+          if (typeof results[i].data.data[0] !== 'undefined') {
+            const city = results[i].data.data[0].cityFrom
+            const trips_by_city = results[i].data.data.map(trip => {
+              return {
+                cityFrom: trip.cityFrom,
+                cityTo: trip.cityTo,
+                price: trip.price,
+                local_departure: trip.local_departure,
+                local_arrival: trip.local_arrival
+              }
+            })
+            trips[city] = trips_by_city
+          }
+          
         }
 
         //Recuperer une liste des destinations communes
-        let commonTrips;
+        let commonTrips = [];
         for (let i = 1; i < cities.length; i++) {
-          let city1 = cities[i].name
-          let city2 = cities[i - 1].name
-          commonTrips = lodash.intersectionBy(trips[city1], trips[city2], 'cityTo');
+          let city1 = cities[i - 1].name
+          let city2 = cities[i].name
+          if (cities.length === 1 ) {
+            commonTrips = lodash.intersectionBy(trips[city1], trips[city2], 'cityTo');
+          } else if(city1 in trips){
+            commonTrips = trips[city1];
+          }
+          
         }
         const commonDestinations = commonTrips.map((item) => {
           return item.cityTo
@@ -49,15 +67,19 @@ export const onClick = (cities, dateFrom, dateTo) => {
         //Retirer les voages qui ne font pas parti des destinations communes
         for (let i = 0; i < cities.length; i++) {
           let city = cities[i].name
-          trips[city] = trips[city].filter((trip) => {
-            return commonDestinations.includes(trip.cityTo)
-          })          
+          if (city in trips) {
+            trips[city] = trips[city].filter((trip) => {
+              return commonDestinations.includes(trip.cityTo)
+            }) 
+          }
+                   
         }
 
         const data = {
           commonDestinations: commonDestinations,
           trips: trips
         }
+        console.log(data)
         dispatch({ type: 'SEARCH', data })
         history.push('/results')
       })
