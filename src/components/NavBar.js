7d3043ece7fe82100e0742b4b255ea570557f516
louisburette <louis.burette@ieseg.fr>
2021-02-14 18:08:34 +0100

    new search on header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,136 @@
 import React from 'react';
 import './_NavBar.scss';
 import logo from '../img/logo.png';
+import FormSearch from './FormSearch';
+import Map from './Map';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 
-export default class NavBar extends React.Component {
+export default class NavBarHome extends React.Component {
   state = {
-    email: '',
-    show: false,
+    isHomePage: true,
+    citiesFrom: [],
+    address: ''
   };
 
-  onEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
+  getFormattedCoordinate = (coordinates) => {
+    coordinates.lat = coordinates.lat.toFixed(6);
+    coordinates.lng = coordinates.lng.toFixed(6);
+    return `${coordinates.lat}-${coordinates.lng}-50km`;
+  };
 
-  resetForm() {
-    this.setState({ email: '' });
-  }
+  findCityIndex = (cityName, cities) => {
+    for (let i = 0; i < cities.length; i++) {
+      if (cities[i].name === cityName) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  handleAddressChange = (address) => {
+    this.setState({
+      address: address,
+    });
+
+    const input = document.querySelector('.city-departure-input');
+    input.addEventListener(
+      'keydown',
+      (event) => {
+        const places = Array.from(
+          event.target.parentElement.querySelectorAll('div[role="option"]')
+        ).map((e) => e.innerText.trim().toLocaleLowerCase());
+
+        if (event.key === 'Enter' && !places.includes(input.value.toLocaleLowerCase())) {
+          if (0 < places.length) {
+            input.value = places[0];
+            this.setState({ address: places[0] });
+          } else {
+            event.stopPropagation();
+            event.preventDefault();
+          }
+        }
+      },
+      true
+    );
+  };
+
+  addCity = async (address) => {
+    const position = await geocodeByAddress(address);
+    const coordinates = await getLatLng(position[0]);
+    const coordinatesFormatted = this.getFormattedCoordinate(coordinates);
+    const cityName = position[0].address_components[0].long_name;
+    const cities = [...this.state.citiesFrom];
+    const cityIndex = this.findCityIndex(cityName, cities);
+
+    if (cityIndex === -1) {
+      //CREATE new city
+      cities.push({
+        name: cityName,
+        lat: coordinates.lat,
+        lng: coordinates.lng,
+        coordinates: coordinatesFormatted,
+        numberOfPeople: 1,
+        showButton: false,
+      });
+    } else {
+      //Add traveler
+      cities[cityIndex].numberOfPeople++;
+    }
+    this.setState({
+      citiesFrom: cities,
+      address: '',
+    });
+  };
+
+  removeCity = (event, index) => {
+    event.preventDefault();
+    const citiesFrom = [...this.state.citiesFrom];
+    citiesFrom.splice(index, 1);
+    this.setState({ citiesFrom });
+  };
 
   render() {
     return (
-      <div className="navbar">
-        <div className="navbar-brand">
-          <a href="/">
-            <img src={logo} alt="Pading find the perfect place to meet" />
-          </a>
+      <div className="header">
+        <div className="navbar">
+          <div className="navbar-menu">
+            <div className="navbar-brand">
+              <a href="/">
+                <img src={logo} alt="Pading find the perfect place to meet" />
+              </a>
+            </div>
+            <div className="navbar-social">
+              <a
+                href="https://www.instagram.com/padingapp/?hl=fr"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div className="value-prop">
+            <h1>Trains and flights combinations for people traveling from multiple places</h1>
+            <p><b>Enter your different departure cities, explore all common destinations to meet at the lowest fare</b></p>
+          </div>
         </div>
-        <div className="navbar-menu">
-          <div className="navbar-social">
-            <a
-              href="https://www.facebook.com/Pading-103563007939325/?view_public_for=103563007939325"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a
-              href="https://www.instagram.com/padingapp/?hl=fr"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="fab fa-instagram"></i>
-            </a>
+        <div className="form">
+          <div className="formsearch">
+            <FormSearch 
+            searchTrips={this.props.searchTrips}
+            isHomePage={this.state.isHomePage}
+            addCity={this.addCity}
+            removeCity={this.removeCity}
+            citiesFrom={this.state.citiesFrom}
+            handleAddressChange={this.handleAddressChange}
+            address={this.state.address}/>
+          </div>
+          <div className="map">
+            <Map
+              citiesFrom={this.state.citiesFrom}
+              citiesTo={[]}
+            />
           </div>
         </div>
       </div>
--- src/routes/Search/components/Why.js
+++ src/routes/Search/components/Why.js
@@ -1,57 +1,59 @@
 import React from 'react';
+import happyCatsGif from '../img/happycats.gif';
+import happyCatsVideo from '../img/happycats.mp4';
 import './_Why.scss';
-import happycats from '../img/happycats.mp4';
 
 export default function Why() {
-  return (
-    <div className="wrapper">
-      <div className="why-to-use-it">
-        <div className="why-to-use-it-gif">
-          <video className="why-to-use-it-video" autoPlay="autoplay" loop muted playsInline>
-            <source src={happycats} />
-          </video>
+
+    let isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+
+    return (
+        <div className="wrapper">
+            <div className="why-to-use-it">
+                <div className="why-to-use-it-gif">
+                    { isIOS ? 
+                    <div><img src={happyCatsGif} className="happy-cats"/></div>
+                    :
+                    <video className="happy-cats" autoPlay="autoplay" loop muted playsInline>
+                        <source src={happyCatsVideo} />
+                    </video>
+                    }
+                </div>
+                <div className="why-to-use-it-text" id="why">
+                    <div className="why-to-use-it-title">
+                        <h2>Why use pading ?</h2>
+                    </div>
+                    <div className="why-to-use-it-content">
+                        <div className="why-to-use-it-content-card">           
+                            <div className="why-to-use-it-number">
+                                <h2>1</h2>
+                            </div>
+                            <div className="why-to-use-it-details">
+                                <h3>Save Time</h3>
+                                <p><b className="focus-text"><span className="underline"></span>In just one search</b>, you and your team can find your next common destination</p>
+                            </div>
+                        </div>
+                        <div className="why-to-use-it-content-card">
+                            <div className="why-to-use-it-number">
+                                <h2>2</h2>
+                            </div>
+                            <div className="why-to-use-it-details">
+                                <h3>Save Money</h3>
+                                <p>Explore the <b className="focus-text"><span className="underline"></span>cheapest destinations</b> with people you plan to meet</p>
+                            </div>
+                        </div>
+                        <div className="why-to-use-it-content-card">
+                            <div className="why-to-use-it-number">
+                                <h2>3</h2>
+                            </div>
+                            <div className="why-to-use-it-details">
+                                <h3>Book Easily</h3>
+                                <p>Share trip details with your team, <b className="focus-text"><span className="underline"></span>book easily</b>... travel and meet</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="why-to-use-it-text" id="why">
-          <div className="why-to-use-it-title">
-            <h2>Why use pading?</h2>
-          </div>
-          <div className="why-to-use-it-content">
-            <div className="why-to-use-it-content-card">
-              <div className="why-to-use-it-number">
-                <h2>1</h2>
-              </div>
-              <div className="why-to-use-it-details">
-                <h3>Save Time</h3>
-                <p>
-                  <b>In just one search</b>, you and your team can find your next common destination
-                </p>
-              </div>
-            </div>
-            <div className="why-to-use-it-content-card">
-              <div className="why-to-use-it-number">
-                <h2>2</h2>
-              </div>
-              <div className="why-to-use-it-details">
-                <h3>Save Money</h3>
-                <p>
-                  <b>Explore the cheapest destinations</b> with people you plan to meet
-                </p>
-              </div>
-            </div>
-            <div className="why-to-use-it-content-card">
-              <div className="why-to-use-it-number">
-                <h2>3</h2>
-              </div>
-              <div className="why-to-use-it-details">
-                <h3>Book Easily</h3>
-                <p>
-                  Share trip details with your team,<b> book easily... travel and meet</b>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
