3f4ab5ffb78b2f622e1a44796b4af40194fdbb59
Louis Burette <louis.burette@ESFRLTA006.local>
2021-04-22 18:48:39 +0200

    ariline names

--- src/routes/Results/components/FlightInfo.js
+++ src/routes/Results/components/FlightInfo.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import './_FlightInfo.scss';
 import './_DetailsResults.scss';
 import moment from 'moment';
+import AvFastForward from 'material-ui/svg-icons/av/fast-forward';
+import { FALSE } from 'node-sass';
 
 export default function FlightInfo(props) {
   const [showDetailsWay, setDetailsWay] = useState(false);
+  const airlines = {
+    "U2": "EasyJet",
+    "AF": "Air France",
+    "FA": "Ryanair"
+  }
 
   const getDuration = (departure_time, arrival_time) => {
     const diff = moment(arrival_time).diff(moment(departure_time));
@@ -19,8 +26,13 @@ export default function FlightInfo(props) {
     return duration;
   };
 
+  const getAirlineName = (airline_code) => {
+    return airlines[airline_code];
+  }
+
   return (
     <div className="details-results-travel-content">
+      {console.log(props)}
       <div className="details-results-travel-content-section">
         <div className="date-div-1"></div>
         <div className="date-div-2">
@@ -83,7 +95,7 @@ export default function FlightInfo(props) {
                     <h6>Airline</h6>
                   </div>
                   <div className="airline-response">
-                    <h6>Ryanair</h6>
+                    <h6>{getAirlineName(props.route.airline)}</h6>
                   </div>
                 </div>
                 <div className="flight-number">
@@ -92,7 +104,7 @@ export default function FlightInfo(props) {
                     <h6>Flight no</h6>
                   </div>
                   <div className="flight-number-response">
-                    <h6>FR7504</h6>
+                    <h6>{props.route.operating_carrier}{props.route.operating_flight_no}</h6>
                   </div>
                 </div>
               </div>
